4629b0d3c2e1cefa99c8359c1775d6e7db7e6b3d
Seth Buntin <seth.buntin@gmail.com>
2011-04-27 16:19:49 -0500

    bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-salmonella",
-    version="0.2.2",
+    version="0.2.3",
     author='Lincoln Loop',
     author_email='info@lincolnloop.com',
     description=("raw_id_fields widget replacement that handles display of an object's "
