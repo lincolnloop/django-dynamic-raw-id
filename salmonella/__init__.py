85deb76c42ec4d4296c4a2bd04c91f3f6d2c7c72
Brandon Konkle <brandon.konkle@gmail.com>
2011-09-29 14:06:31 -0500

    Edited salmonella/__init__.py via GitHub

---
+++ salmonella/__init__.py
@@ -0,0 +1,17 @@
+VERSION = (0, 4, 3, "a", 1) # following PEP 386
+DEV_N = 1 # for PyPi releases, set this to None
+
+
+def get_version(short=False):
+    version = "%s.%s" % (VERSION[0], VERSION[1])
+    if short:
+        return version
+    if VERSION[2]:
+        version = "%s.%s" % (version, VERSION[2])
+    if VERSION[3] != "f":
+        version = "%s%s%s" % (version, VERSION[3], VERSION[4])
+        if DEV_N:
+            version = "%s.dev%s" % (version, DEV_N)
+    return version
+
+__version__ = get_version()
