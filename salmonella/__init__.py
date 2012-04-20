86909f874012d1795f878ffeaa3bbd9b8e14d5a7
Seth Buntin <seth.buntin@gmail.com>
2012-04-20 09:08:51 -0500

    bump version to 0.5.1 development version

--- salmonella/__init__.py
+++ salmonella/__init__.py
@@ -1,5 +1,5 @@
-VERSION = (0, 5, 0, "f")  # following PEP 386
-DEV_N = None  # for PyPi releases, set this to None
+VERSION = (0, 5, 1, "a")  # following PEP 386
+DEV_N = 1  # for PyPi releases, set this to None
 
 
 def get_version(short=False):
