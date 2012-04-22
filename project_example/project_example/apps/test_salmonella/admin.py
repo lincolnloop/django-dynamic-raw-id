7c55f233c5fd4fd53413d683ab924157daa5eab8
Martin Mahner <martin@mahner.org>
2012-04-22 15:12:32 +0200

    Added a many2many field to the test project.

--- project_example/project_example/apps/test_salmonella/admin.py
+++ project_example/project_example/apps/test_salmonella/admin.py
@@ -6,7 +6,7 @@ from .models import SalmonellaTest
 
 
 class SalmonellaTestAdmin(SalmonellaMixin, admin.ModelAdmin):
-    salmonella_fields = ('user', 'staff_member')
+    salmonella_fields = ('user', 'staff_member', 'staff_member_many')
 
 
 admin.site.register(SalmonellaTest, SalmonellaTestAdmin)
--- project_example/project_example/apps/test_salmonella/models.py
+++ project_example/project_example/apps/test_salmonella/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-
 class SalmonellaTest(models.Model):
     user = models.ForeignKey('auth.User')
     staff_member = models.ForeignKey('auth.User',
         related_name='staff_members', limit_choices_to={'is_staff': True})
+    staff_member_many = models.ManyToManyField('auth.User',
+        related_name='staff_members_many')
