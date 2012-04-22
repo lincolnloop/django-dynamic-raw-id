83dd70a2a876da89a8804ac929ffbab99d75732a
Martin Mahner <martin@mahner.org>
2012-04-22 17:18:55 +0200

    Added a separate check if user has change permission.

--- salmonella/views.py
+++ salmonella/views.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponseBadRequest
+from django.http import HttpResponseBadRequest, HttpResponseForbidden
 from django.shortcuts import render_to_response
 from django.db.models import get_model
 
@@ -32,10 +32,16 @@ def label_view(request, app_name, model_name, template_name="", multi=False,
         msg = 'No list or only invalid ids of objects given'
         return HttpResponseBadRequest(settings.DEBUG and msg or '')
 
+    # Make sure this model exists and the user has 'change' permission for it.
+    # If he doesnt have this permission, Django would not display the
+    # change_list in the popup and the user were never able to select objects.
     model = get_model(app_name, model_name)
     if not model:
         msg = 'Model %s.%s does not exist.' % (app_name, model_name)
         return HttpResponseBadRequest(settings.DEBUG and msg or '')
+
+    if not request.user.has_perm('%s.change_%s' % (app_name, model_name)):
+        return HttpResponseForbidden()
 
     try:
         if multi:
