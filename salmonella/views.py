c34461d1a3830789498759b8ede93224b4d7307f
Martin Mahner <martin@mahner.org>
2012-04-22 16:57:29 +0200

    Added a couple of sanity checks for incoming data.

--- salmonella/views.py
+++ salmonella/views.py
@@ -1,22 +1,46 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
+from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render_to_response
 from django.db.models import get_model
-from django.contrib.auth.decorators import user_passes_test
 
 
 @user_passes_test(lambda u: u.is_staff)
 def label_view(request, app_name, model_name, template_name="", multi=False,
                template_object_name="object"):
 
+    # The list of to obtained objects is in GET.id. No need to resume if we
+    # didnt get it.
+    if not request.GET.get('id'):
+        msg = 'No list of objects given'
+        return HttpResponseBadRequest(settings.DEBUG and msg or '')
+
+    # Given objects are either an integer or a comma-separted list of
+    # integers. Validate them and ignore invalid values. Also strip them
+    # in case the user entered values by hand, such as '1, 2,3'.
+    object_list = []
+    for pk in request.GET['id'].split(","):
+        try:
+            object_list.append(int(pk))
+        # Not an integer, not convertable
+        except ValueError:
+            pass
+
+    # Check if at least one value survived this cleanup. If not,
+    if len(object_list) == 0:
+        msg = 'No list or only invalid ids of objects given'
+        return HttpResponseBadRequest(settings.DEBUG and msg or '')
+
+    model = get_model(app_name, model_name)
+    if not model:
+        msg = 'Model %s.%s does not exist.' % (app_name, model_name)
+        return HttpResponseBadRequest(settings.DEBUG and msg or '')
+
     try:
-        object_id = request.GET.get("id", "")
-        model = get_model(app_name, model_name)
         if multi:
-            if object_id:
-                object_id = object_id.split(",")
             model_template = "salmonella/%s/multi_%s.html" % (app_name, model_name)
-            objs = model.objects.filter(id__in=object_id)
+            objs = model.objects.filter(id__in=object_list)
             objects = []
             for obj in objs:
                 change_url = reverse("admin:%s_%s_change" % (app_name, model_name),
@@ -28,16 +52,14 @@ def label_view(request, app_name, model_name, template_name="", multi=False,
             }
         else:
             model_template = "salmonella/%s/%s.html" % (app_name, model_name)
-            obj = model.objects.get(id=object_id)
+            obj = model.objects.get(id=object_list[0])
             change_url = reverse("admin:%s_%s_change" % (app_name, model_name),
                                  args=[obj.id])
-            obj = (obj, change_url)
-
             extra_context = {
-                template_object_name: obj,
+                template_object_name: (obj, change_url),
             }
     except model.DoesNotExist:
-        return HttpResponse("")
+        msg = 'Model instance does not exist'
+        return HttpResponseBadRequest(settings.DEBUG and msg or '')
 
-    return render_to_response((model_template, template_name),
-                              extra_context)
+    return render_to_response((model_template, template_name), extra_context)
