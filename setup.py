cadd8fa622992261f38eaee4ac476c6f694f85c9
Martin Mahner <martin@mahner.org>
2013-08-07 10:42:29 +0200

    Simplified version number handling

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-VERSION = __import__('salmonella').__version__
-
 setup(
     name="django-salmonella",
-    version=VERSION,
+    version='0.6',
     author='Lincoln Loop: Seth Buntin, Yann Malet',
     author_email='info@lincolnloop.com',
     description=("raw_id_fields widget replacement that handles display of an object's "
                  "string value on change and can be overridden via a template."),
     packages=find_packages(),
-    package_data={'salmonella': [
-        'static/salmonella/js/*.js',
-        'static/salmonella/img/*.gif',
-        'templates/salmonella/*.html',
-        'templates/salmonella/admin/*.html',
-        'templates/salmonella/admin/widgets/*.html'
+    package_data={'slidebox': [
+        'static/*.*', 
+        'templates/*.*'
     ]},
     url="http://github.com/lincolnloop/django-salmonella/",
     classifiers=[
