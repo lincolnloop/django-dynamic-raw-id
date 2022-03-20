1e74143bfecc6ac1d18a7fb4e5f94cf10a95b0c7
Martin Mahner <martin@mahner.org>
2022-03-20 16:08:40 +0100

    Django 3 URLs

--- dynamic_raw_id/tests/testapp/urls.py
+++ dynamic_raw_id/tests/testapp/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"^admin/dynamic_raw_id/", include("dynamic_raw_id.urls")),
+    path("admin/", admin.site.urls),
+    path("admin/dynamic_raw_id/", include("dynamic_raw_id.urls")),
 ]
