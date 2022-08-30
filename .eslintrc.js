78f9d156eeaaa65b9c84656cc7019d8ec8a01168
Hash Minner <e19198@eng.pdn.ac.lk>
2022-08-31 00:42:33 +0530

    improve code formate in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,39 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
     },
-    "sourceType": "module"
+    sourceType: "module",
   },
-  "globals" : {
-    "window": true
+  globals: {
+    window: true,
   },
-  "extends": ["eslint:recommended"],
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      1,
-      "unix"
-    ],
-    "quotes": [
-      1,
-      "single"
-    ],
-    "semi": [
-      1,
-      "always"
-    ],
+  extends: ["eslint:recommended"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": [1, "unix"],
+    quotes: [1, "single"],
+    semi: [1, "always"],
     "eol-last": "error",
-    "comma-spacing": ["error", {"before": false, "after": true}],
+    "comma-spacing": ["error", { before: false, after: true }],
     "space-before-function-paren": ["error", "never"],
     "space-in-parens": ["error", "never"],
     "keyword-spacing": [
-      "error", {
-        "before": true,
-        "after": true,
-        "overrides": {
-          "throw": {
-            "after": false
-          }
-        }
-      }
-    ]
-  }
-};
+      "error",
+      {
+        before: true,
+        after: true,
+        overrides: {
+          throw: {
+            after: false,
+          },
+        },
+      },
+    ],
+  },
+}
