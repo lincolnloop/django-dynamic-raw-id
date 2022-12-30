156a2c89f73de65a4b569e2ee508f20d02809ca4
Alexandro Tapia <zpaceway@gmail.com>
2022-12-30 11:18:37 -0500

    update dynamic raw id to receive the admin site obj

--- dynamic_raw_id/urls.py
+++ dynamic_raw_id/urls.py
@@ -1,10 +1,11 @@
+from django.contrib.admin import site as default_admin_site
 from django.urls import path
 
 from dynamic_raw_id.views import label_view
 
 app_name = 'dynamic_raw_id'
 
-def get_urls(admin_site_name='admin'):
+def get_urls(admin_site=default_admin_site):
     urlpatterns = [
         path(
             "<slug:app_name>/<slug:model_name>/multiple/",
@@ -13,7 +14,7 @@ def get_urls(admin_site_name='admin'):
                 "multi": True,
                 "template_object_name": "objects",
                 "template_name": "dynamic_raw_id/multi_label.html",
-                "admin_site_name": admin_site_name
+                "admin_site": admin_site
             },
             name="dynamic_raw_id_multi_label",
         ),
@@ -22,7 +23,7 @@ def get_urls(admin_site_name='admin'):
             label_view,
             {
                 "template_name": "dynamic_raw_id/label.html",
-                "admin_site_name": admin_site_name
+                "admin_site": admin_site
             },
             name="dynamic_raw_id_label",
         ),
