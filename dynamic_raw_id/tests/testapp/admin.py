a824cb493a04bea8e3ff8e3e541fb3ed9d751a9d
Martin Mahner <martin@mahner.org>
2024-04-21 11:38:37 +0200

    Add test field for UUID pk fields.

--- dynamic_raw_id/tests/testapp/admin.py
+++ dynamic_raw_id/tests/testapp/admin.py
@@ -3,21 +3,32 @@ from django.contrib import admin
 from dynamic_raw_id.admin import DynamicRawIDMixin
 from dynamic_raw_id.filters import DynamicRawIDFilter
 
-from .models import CharPrimaryKeyModel, DirectPrimaryKeyModel, TestModel
+from . import models
 
 
+@admin.register(models.TestModel)
 class TestModelAdmin(DynamicRawIDMixin, admin.ModelAdmin):
-    raw_id_fields = ("rawid_fk", "rawid_fk_limited", "rawid_many")
+    raw_id_fields = (
+        "rawid_fk",
+        "rawid_fk_limited",
+        "rawid_many",
+    )
     dynamic_raw_id_fields = (
         "dynamic_raw_id_fk",
         "dynamic_raw_id_fk_limited",
         "dynamic_raw_id_many",
-        "dynamic_raw_id_fk_direct_pk",
+        "dynamic_raw_id_fk_int_pk",
         "dynamic_raw_id_fk_char_pk",
+        "dynamic_raw_id_fk_uuid_pk",
     )
-    list_filter = (("dynamic_raw_id_fk", DynamicRawIDFilter),)
+    list_filter = (
+        ("dynamic_raw_id_fk", DynamicRawIDFilter),
+        ("dynamic_raw_id_fk_int_pk", DynamicRawIDFilter),
+        ("dynamic_raw_id_fk_char_pk", DynamicRawIDFilter),
+        ("dynamic_raw_id_fk_uuid_pk", DynamicRawIDFilter),
+    )
 
 
-admin.site.register(DirectPrimaryKeyModel)
-admin.site.register(CharPrimaryKeyModel)
-admin.site.register(TestModel, TestModelAdmin)
+admin.site.register(models.IntPrimaryKeyModel)
+admin.site.register(models.CharPrimaryKeyModel)
+admin.site.register(models.UUIDPrimaryKeyModel)
--- dynamic_raw_id/tests/testapp/models.py
+++ dynamic_raw_id/tests/testapp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 
-class DirectPrimaryKeyModel(models.Model):
+# Test custom forms of primary key fields
+class IntPrimaryKeyModel(models.Model):
     num = models.IntegerField("Number", primary_key=True)
 
     def __str__(self) -> str:
@@ -12,10 +13,18 @@ class CharPrimaryKeyModel(models.Model):
     chr = models.CharField(max_length=20, primary_key=True)
 
     def __str__(self) -> str:
-        return self.chr
+        return str(self.chr)
+
+
+class UUIDPrimaryKeyModel(models.Model):
+    uuid = models.UUIDField(primary_key=True)
+
+    def __str__(self) -> str:
+        return str(self.uuid)
 
 
 class TestModel(models.Model):
+    # Regular RawID Fields -------------------------------------------------------------
     rawid_fk = models.ForeignKey(
         "auth.User",
         related_name="rawid_fk",
@@ -23,7 +32,6 @@ class TestModel(models.Model):
         null=True,
         on_delete=models.CASCADE,
     )
-
     rawid_fk_limited = models.ForeignKey(
         "auth.User",
         related_name="rawid_fk_limited",
@@ -32,19 +40,11 @@ class TestModel(models.Model):
         null=True,
         on_delete=models.CASCADE,
     )
-
     rawid_many = models.ManyToManyField(
         "auth.User", related_name="rawid_many", blank=True
     )
 
-    rawid_fk_direct_pk = models.ForeignKey(
-        DirectPrimaryKeyModel,
-        related_name="rawid_fk_direct_pk",
-        blank=True,
-        null=True,
-        on_delete=models.CASCADE,
-    )
-
+    # Dynamic RawID Fields with Django Default PK --------------------------------------
     dynamic_raw_id_fk = models.ForeignKey(
         "auth.User",
         related_name="dynamic_raw_id_fk",
@@ -52,7 +52,6 @@ class TestModel(models.Model):
         null=True,
         on_delete=models.CASCADE,
     )
-
     dynamic_raw_id_fk_limited = models.ForeignKey(
         "auth.User",
         related_name="dynamic_raw_id_fk_limited",
@@ -61,19 +60,21 @@ class TestModel(models.Model):
         null=True,
         on_delete=models.CASCADE,
     )
-
     dynamic_raw_id_many = models.ManyToManyField(
         "auth.User", related_name="dynamic_raw_id_many", blank=True
     )
 
-    dynamic_raw_id_fk_direct_pk = models.ForeignKey(
-        DirectPrimaryKeyModel,
-        related_name="dynamic_raw_id_fk_direct_pk",
+    # Dynamic RawID Fields with Custom PK Fields ---------------------------------------
+    # Custom PrimaryKey Field Model with a Integer value
+    dynamic_raw_id_fk_int_pk = models.ForeignKey(
+        IntPrimaryKeyModel,
+        related_name="dynamic_raw_id_fk_int_pk",
         blank=True,
         null=True,
         on_delete=models.CASCADE,
     )
 
+    # Custom PrimaryKey Field Model with a Character value
     dynamic_raw_id_fk_char_pk = models.ForeignKey(
         CharPrimaryKeyModel,
         related_name="dynamic_raw_id_fk_char_pk",
@@ -82,7 +83,14 @@ class TestModel(models.Model):
         on_delete=models.CASCADE,
     )
 
+    # Custom PrimaryKey Field Model with an UUID value
+    dynamic_raw_id_fk_uuid_pk = models.ForeignKey(
+        UUIDPrimaryKeyModel,
+        related_name="dynamic_raw_id_fk_uuid_pk",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
     def __str__(self) -> str:
-        if self.dynamic_raw_id_fk:
-            return self.dynamic_raw_id_fk.username
-        return "Unnamed Test Instance"
+        return "Test Instance"
